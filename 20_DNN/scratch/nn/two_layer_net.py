7a89ead983f8eba1105f5776cb1c378dd90fab25
harperfu6 <beharp8a8@gmail.com>
2019-09-09 07:36:22 +0000

    add layers

--- 20_DNN/scratch/nn/two_layer_net.py
+++ 20_DNN/scratch/nn/two_layer_net.py
@@ -1,11 +1,14 @@
 # coding: utf-8
 import sys, os
 sys.path.append(os.pardir)
-from common.functions import *
+
+import numpy as np
+from common.layers import *
 from common.gradient import numerical_gradient
+from collections import OrderedDict
+
 
 class TwoLayerNet:
-
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         # 重みの初期化
         self.params = {}
@@ -14,33 +17,60 @@ class TwoLayerNet:
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
+        # レイヤの生成
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+        self.lastlayer = SoftmaxWithLoss()
+
     def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-
-        return y
-
-    # x:入力データ，t:教師データ
+    # x:入力データ, t:教師データ
     def loss(self, x, t):
         y = self.predict(x)
-        return cross_entropy_error(y, t)
+        return self.lastlayer.forward(y, t)
+
+    def gradient(self, x, t):
+        # forward
+        # それぞれのレイヤにforwardしてbackwardで必要な入力値を設定する
+        self.loss(x, t)
+
+        # backward
+        dout = 1 # softmaxの正解データは1であるため
+        dout = self.lastlayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 設定
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
+
 
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
 
-        accuracy = np.sum(y==t) / float(x.shape[0])
+        # tがonehotならば，indexを取得
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
 
-    # x:入力データ，t:教師データ
+
+    # 数値微分
     def numerical_gradient(self, x, t):
-        loss_W = lambda W: self.loss(x, t) # 無名関数，引数のWは意味を持たない
+        loss_W = lambda W: self.loss(x, t)
 
         grads = {}
         grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
@@ -49,29 +79,3 @@ class TwoLayerNet:
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
 
         return grads
-
-    def gradient(self, x, t):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
-        grads = {}
-
-        batch_num = x.shape[0]
-
-        # forward
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-
-        # backward
-        dy = (y - t) / batch_num
-        grads['W2'] = np.dot(z1.T, dy)
-        grads['b2'] = np.sum(dy, axis=0)
-
-        dz1 = np.dot(dy, W2.T)
-        da1 = sigmoid_grad(a1) * dz1
-        grads['W1'] = np.dot(x.T, da1)
-        grads['b1'] = np.sum(da1, axis=0)
-
-        return grads
-    
